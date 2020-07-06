010120fb468ad6ee8ed87e150c78badcc478362f
Andy <andyalam9@gmail.com>
2020-07-05 19:18:39 -0700

    hw2: base api call completed

--- hw2/src/main/java/com/example/hw2/service/CityService.java
+++ hw2/src/main/java/com/example/hw2/service/CityService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.TimeZone;
 
 import com.example.hw2.domain.CityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import com.example.hw2.domain.*;
 
 @Service
 public class CityService {
+	private static final Logger log = LoggerFactory.getLogger(CityService.class);
 	
 	@Autowired
 	private CityRepository cityRepository;
@@ -24,10 +27,11 @@ public class CityService {
 	private WeatherService weatherService;
 	
 	public CityInfo getCityInfo(String cityName) {
-		
-		// TODO your code goes here
-		// delete the following line
-		return null; 
+		City city = cityRepository.findByName(cityName).get(0);
+		Country country = countryRepository.findByCode(city.getCountryCode());
+		TempAndTime tempAndTime = weatherService.getTempAndTime(cityName);
+
+		return new CityInfo(city, country.getName(), tempAndTime.temp, "time_here");
 	}
 	
 }
--- hw2/src/main/java/com/example/hw2/service/WeatherService.java
+++ hw2/src/main/java/com/example/hw2/service/WeatherService.java
@@ -25,12 +25,60 @@ public class WeatherService {
 		this.apiKey = apiKey; 
 	}
 	
-	public  TempAndTime getTempAndTime(String cityName) {
+	public TempAndTime getTempAndTime(String cityName) {
 		ResponseEntity<JsonNode> response = restTemplate.getForEntity(
 				weatherUrl + "?q=" + cityName + "&appid=" + apiKey,
 				JsonNode.class);
 		JsonNode json = response.getBody();
 		log.info("Status code from weather server:" + response.getStatusCodeValue());
+		log.info("JSON response from weather service:" + json.toString());
+
+		/*
+		{
+		  "coord":{
+			"lon":-119.83,
+			"lat":36.67
+		  },
+		  "weather":[
+			{
+			  "id":801,
+			  "main":"Clouds",
+			  "description":"few clouds",
+			  "icon":"02d"
+			}
+		  ],
+		  "base":"stations",
+		  "main":{
+			"temp":310.63,
+			"feels_like":305.25,
+			"temp_min":310.37,
+			"temp_max":311.15,
+			"pressure":1011,
+			"humidity":14
+		  },
+		  "visibility":16093,
+		  "wind":{
+			"speed":6.2,
+			"deg":310
+		  },
+		  "clouds":{
+			"all":20
+		  },
+		  "dt":1593994942,
+		  "sys":{
+			"type":1,
+			"id":4056,
+			"country":"US",
+			"sunrise":1593953188,
+			"sunset":1594005672
+		  },
+		  "timezone":-25200,
+		  "id":5350964,
+		  "name":"Fresno",
+		  "cod":200
+		}
+		*/
+
 		double temp = json.get("main").get("temp").asDouble();
 		long time = json.get("dt").asLong();
 		int timezone = json.get("timezone").asInt();
