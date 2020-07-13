7277d4546d0b30cd7edc16576caa68e3faa949f2
Andy <andyalam9@gmail.com>
2020-07-12 21:01:37 -0700

    rabbitmq tutorial

---
+++ rabbitmq-tutorial/src/main/java/com/example/rabbitmqtutorial/TutlConfig.java
@@ -0,0 +1,28 @@
+package com.example.rabbitmqtutorial;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({ "tutl", "hello-world" })
+@Configuration
+public class TutlConfig {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Tut1Receiver receiver() {
+        return new Tut1Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut1Sender sender() {
+        return new Tut1Sender();
+    }
+}
